9576bce2efda84686f78aa0c242ff16a04031ccd
Stolkerve <63207265+Stolkerve@users.noreply.github.com>
2023-02-01 13:16:51 -0400

    layout del menu principal

--- src/main/java/com/proyecto1/gui/MainPanel.java
+++ src/main/java/com/proyecto1/gui/MainPanel.java
@@ -1,10 +1,20 @@
 package com.proyecto1.gui;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingWorker;
+
 import com.proyecto1.utils.AssetsManager;
 import com.proyecto1.utils.GraphFile;
 import com.proyecto1.utils.ImageAsset;
-
-import javax.swing.*;
 
 /**
  * @author sebas
@@ -23,11 +33,33 @@ public class MainPanel extends javax.swing.JPanel {
     }
 
     private void initComponents() {
+        this.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        JPanel colsPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        JPanel leftCol = new JPanel(new GridLayout(3, 0));
+        //leftCol.setBackground(new Color(0x95ffff));
+
+        JPanel centerCol = new JPanel(new GridLayout(3, 0));
+
+        JPanel rightCol = new JPanel(new GridLayout(3, 0));
+
         ImageAsset catKiss = AssetsManager.getInstance().getImage("cat-kiss");
         if (catKiss != null) {
-            JLabel label = new JLabel(catKiss.image);
-            this.add(label);
+            for (int i = 0; i < 3; i++) {
+                JPanel imgPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+                imgPanel.add(new JLabel(catKiss.image));
+                leftCol.add(imgPanel);
+            }
+
+            for (int i = 0; i < 3; i++) {
+                JPanel imgPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+                imgPanel.add(new JLabel(catKiss.image));
+                rightCol.add(imgPanel);
+            }
         }
+
         JButton cargarArchivoBtn = new JButton("Cargar archivo de almacenes");
         cargarArchivoBtn.addActionListener(e -> {
             cargarArchivoBtn.setEnabled(false);
@@ -45,6 +77,18 @@ public class MainPanel extends javax.swing.JPanel {
             };
             sw.execute();
         });
-        this.add(cargarArchivoBtn);
+        centerCol.add(cargarArchivoBtn);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.ipadx = 80;
+        colsPanel.add(leftCol, c);
+        c.gridx = 1;
+        c.ipadx = 80;
+        colsPanel.add(centerCol, c);
+        c.gridx = 2;
+        colsPanel.add(rightCol,c);
+
+        this.add(colsPanel);
     }
 }
